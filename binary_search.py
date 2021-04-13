858d504a28815be4ab2b31f19a5fb7788961ef2f
HamiltonMultimedia <24661651+HamiltonMultimedia@users.noreply.github.com>
2021-04-12 20:24:15 -0400

    Added binary_search.py.

--- binary_search.py
+++ binary_search.py
@@ -8,7 +8,7 @@ import time
 # This is used to speed up searching through a sorted list. 
 # Binary search algo. ~ 0(log(n))
 
-def binary_search(l, target, low=None, high=None):
+def binary_search(l, target, low = None, high = None):
     if low is None:
         low = 0
     if high is None:
@@ -17,20 +17,20 @@ def binary_search(l, target, low=None, high=None):
     if high < low:
         return -1
 
-# if l = [1, 3, 5, 10, 12] it should return 3
-midpoint = (low + high) // 2 
+    # if l = [1, 3, 5, 10, 12] it should return 3
+    midpoint = (low + high) // 2 
 
-# Now we'll test if l[midpoint] == target, 
-# If not we'll find out if the target is on the left or the right of the the midpoint
-# The algo tells us everything to the left of the midpoint is smaller that the midpoint
-# and everything to the right will be bigger
-if l[midpoint] == target:
-    return midpoint
-elif target < l[midpoint]:
-    return binary_search(l, target, low, midpoint-1)
-else:
-# At this point target > l[midpoint]
-    return binary_search(l, target, midpoint+1, high)
+    # Now we'll test if l[midpoint] == target, 
+    # If not we'll find out if the target is on the left or the right of the the midpoint
+    # The algo tells us everything to the left of the midpoint is smaller that the midpoint
+    # and everything to the right will be bigger
+    if l[midpoint] == target:
+        return midpoint
+    elif target < l[midpoint]:
+        return binary_search(l, target, low, midpoint-1)
+    else:
+    # At this point target > l[midpoint]
+        return binary_search(l, target, midpoint+1, high)
 
 if __name__=='__main__':
     # Remember l = [1, 3, 5, 10, 12]
@@ -43,4 +43,10 @@ if __name__=='__main__':
     while len(sorted_list) < length:
         sorted_list.add(random.randint(-3*length, 3*length))
     sorted_list = sorted(list(sorted_list))
+
+    start = time.time()
+    for target in sorted_list:
+        binary_search(sorted_list, target)
+    end = time.time()
+    print("Binary search time: ", (end - start), "seconds")
 
